2e4e61e21be20b276da6a96a8d6807fae5eda248
Nickolas <nickolas.oliveira02@gmail.com>
2024-11-09 11:30:24 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,10 +3,22 @@ import { transactionsColumns } from "./_columns";
 import { DataTable } from "../_components/ui/data-table";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   // Acessar as transações do banco de dados
-  const transactions = await db.transaction.findMany({});
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
 
   return (
     <>
